48fb829ad36a2ad08d171596b7d1aeef2d0268da
Andreii Pozniakov <immutableperson@gmail.com>
2022-07-14 17:54:48 +0300

    fix: remove nav from social media links

--- src/components/Layout/components/Footer/components/FooterSocialMedia/index.js
+++ src/components/Layout/components/Footer/components/FooterSocialMedia/index.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 import Icon from '~components/Icon'
 import NavSocialMedia from '~constants/navSocialMedia'
 
+import * as s from './FooterSocialMedia.module.scss'
+
 const FooterSocialMedia = (props) => {
   const { className, ...rest } = props
 
   return (
-    <Nav {...rest} variant="footer-social" as="ul">
+    <div {...rest} className={s.social}>
       {NavSocialMedia.map(({ iconName, title, href }) => (
-        <Nav.Item key={title} as="li">
-          <Nav.Link
-            href={href}
-            title={title}
-            aria-label={title}
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-          >
-            <Icon name={iconName} size={20} />
-          </Nav.Link>
-        </Nav.Item>
+        <a
+          href={href}
+          title={title}
+          aria-label={title}
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          className={s.socialLink}
+        >
+          <Icon name={iconName} size={20} />
+        </a>
       ))}
-    </Nav>
+    </div>
   )
 }
 
