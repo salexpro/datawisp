8bfa971a2d35d32005f94d4e2b5f6f408366d725
Savelii Lukash <SaveliiLukash@gmail.com>
2023-01-11 18:13:40 +0200

    feat: hide `/blog` page tabs

--- src/containers/Blog/components/BlogTabs/index.js
+++ src/containers/Blog/components/BlogTabs/index.js
@@ -1,32 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useLocation } from '@gatsbyjs/reach-router'
-import { Link } from 'gatsby'
-import { Container, Nav } from 'react-bootstrap'
+// import { useLocation } from '@gatsbyjs/reach-router'
+// import { Link } from 'gatsby'
+import {
+  Container,
+  // Nav
+} from 'react-bootstrap'
 
 import * as s from './BlogTabs.module.scss'
 
 const BlogTabs = (props) => {
   const { heading, tabs, className, ...rest } = props
 
-  const { hash } = useLocation()
-  const activeKey = hash.replace('#', '')
+  // const { hash } = useLocation()
+  // const activeKey = hash.replace('#', '')
 
   return (
     <Container {...rest} className={className}>
       <h1 className={s.heading}>{heading}</h1>
-      <nav className="nav-blog-wrapper">
-        <Nav variant="blog" as="ul" activeKey={activeKey}>
-          {tabs.map(({ name, slug }) => (
-            <Nav.Item key={slug} as="li">
-              <Nav.Link eventKey={slug} as={Link} to={`#${slug}`}>
-                {name}
-              </Nav.Link>
-            </Nav.Item>
-          ))}
-        </Nav>
-      </nav>
+      {/* <nav className="nav-blog-wrapper"> */}
+      {/*   <Nav variant="blog" as="ul" activeKey={activeKey}> */}
+      {/*     {tabs.map(({ name, slug }) => ( */}
+      {/*       <Nav.Item key={slug} as="li"> */}
+      {/*         <Nav.Link eventKey={slug} as={Link} to={`#${slug}`}> */}
+      {/*           {name} */}
+      {/*         </Nav.Link> */}
+      {/*       </Nav.Item> */}
+      {/*     ))} */}
+      {/*   </Nav> */}
+      {/* </nav> */}
     </Container>
   )
 }
