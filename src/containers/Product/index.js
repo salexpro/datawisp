4cafd3f59fb211aadf5b8c177c2db2b49adae2eb
Alex <dev@salex.pro>
2024-10-01 13:38:47 +0300

    feat: update product page
    add How section & update Features

--- src/containers/Product/index.js
+++ src/containers/Product/index.js
@@ -6,16 +6,18 @@ import SectionIntegrations from '~components/SectionIntegrations'
 import SectionFeatures from '~components/SectionFeatures'
 import SectionHero from './components/SectionHero'
 import SectionBlocks from './components/SectionBlocks'
+import SectionHow from './components/SectionHow'
 
 const Product = (props) => {
-  const { hero, features, integrations, functions, utm } = props
+  const { hero, how, features, integrations, functions, utm } = props
 
   return (
     <>
       <SectionHero {...hero} utm={utm} />
+      <SectionHow {...how} />
       <SectionBlocks {...functions} />
       <SectionIntegrations {...integrations} />
-      <SectionFeatures {...features} utm={utm} variant="long" />
+      <SectionFeatures {...features} utm={utm} variant="product" />
     </>
   )
 }
